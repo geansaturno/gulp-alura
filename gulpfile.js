e9681817999067f61d64ede2373e6092ed17c77f
Gean Saturno <cronosnaeg@gmail.com>
2017-05-19 16:28:20 -0300

    aula 03

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,37 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var clean = require('gulp-clean');
+var gulp        = require('gulp');
+var imagemin    = require('gulp-imagemin');
+var clean       = require('gulp-clean');
+var usemin      = require('gulp-usemin');
+var uglify      = require('gulp-uglify');
+var cssmin      = require('gulp-cssmin');
+var concat      = require('gulp-concat');
+var htmlReplace = require('gulp-html-replace');
 
 gulp.task('clean', function(){
-    return gulp.src('/dist')
+    return gulp.src('dist')
         .pipe(clean());
 });
 
 gulp.task('copy', ['clean'], function(){
     return gulp.src('src/**/*')
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build-img', ['copy'], function(){
+gulp.task('build-img', function(){
     gulp.src('src/img/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('src/img-min'));
+        .pipe(imagemin())
+            .pipe(gulp.dest('src/img-min'));
+});
+
+gulp.task('usemin', function(){
+    gulp.src('dist/**/*.html')
+        .pipe(usemin({
+            js: [uglify],
+            css: [cssmin]
+        }))
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['copy'], function(){
+    gulp.start('build-img', 'usemin');
 });
