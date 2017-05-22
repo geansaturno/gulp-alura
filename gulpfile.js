2650129011e5436424dac95fb14f38484b9c03f5
Gean Saturno <cronosnaeg@gmail.com>
2017-05-22 14:15:37 -0300

    Aula 07

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var jshintStylish   = require('jshint-stylish');
 var csslint         = require('gulp-csslint');
 var csslintStylish  = require('csslint-stylish');
 var autoPrefixer    = require('gulp-autoprefixer');
+var less            = require('gulp-less');
 
 gulp.task('clean', function(){
     return gulp.src('dist')
@@ -51,14 +52,25 @@ gulp.task('server', function(){
 
     gulp.watch('src/js/*.js', function(event){
         gulp.src(event.path)
-        .pipe(jsHint())
-            .pipe(jsHint.reporter(jshintStylish));
+            .pipe(jsHint())
+                .pipe(jsHint.reporter(jshintStylish));
     });
 
     gulp.watch('src/css/*.css', function(event){
         gulp.src(event.path)
-        .pipe(csslint())
-            .pipe(csslint.reporter());
+            .pipe(csslint())
+                .pipe(csslint.reporter());
+    });
+
+    gulp.watch('src/less/*.less', function(event){
+        console.log('compilando ', event.path);
+        gulp.src(event.path)
+            .pipe(less())
+                .on('error', function(error){
+                    console.error("Erro em ", error.filename);
+                    console.error(error.message);
+                })
+                    .pipe(gulp.dest('src/css'));
     });
 
     gulp.watch('src/**/*', browserSync.reload);
